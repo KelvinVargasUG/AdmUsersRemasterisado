7e1cc2b4d5a1542345fef067aaf68f0d7027632b
KelvinVargasUG <joel.vargas.ch@hotmail.com>
2023-10-21 11:04:45 -0500

    acutalizacion de script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -176,6 +176,14 @@ END habilitar_usuario;
 
 
 
+--cambar rol usuario--------------------------------------------------------------------
+create or replace procedure update_rol_user(p_id_rol in number,p_id_user in number) as
+begin
+update KJVARGAS.USUARIO_ROL u set u.id_rol = p_id_rol where u.ID_USUARIO = p_id_user;
+end;
+
+
+
 --find all rol--------------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE kjvargas.find_all_rol(p_rol OUT SYS_REFCURSOR)
     IS
