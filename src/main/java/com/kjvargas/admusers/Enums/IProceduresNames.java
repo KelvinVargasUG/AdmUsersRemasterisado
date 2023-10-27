f10118eb438569207701a0afb25e4c497429858a
VARGAS CHAFLE KELVIN JOSUE <kvargas@sasf.local>
2023-10-27 16:07:58 -0500

    enums actualizado

--- src/main/java/com/kjvargas/admusers/Enums/IProceduresNames.java
+++ src/main/java/com/kjvargas/admusers/Enums/IProceduresNames.java
@@ -1,29 +1,32 @@
 package com.kjvargas.admusers.Enums;
 
 public interface IProceduresNames {
-    String FIND_ALL_USERS = "kjvargas.find_all_users";
 
-    String CREATE_ADMIN_USER = "kjvargas.createAdminUser";
+    static final String SCHEMA = "kjvargas.";
 
-    String DELETE_USUARIO = "kjvargas.deleteUsuario";
+    String FIND_ALL_USERS = SCHEMA+"find_all_users";
 
-    String HABILITAR_USUARIO = "kjvargas.habilitar_usuario";
+    String CREATE_ADMIN_USER = SCHEMA+"createAdminUser";
 
-    String CREATE_USUARIO = "kjvargas.createUsuario";
+    String DELETE_USUARIO = SCHEMA+"deleteUsuario";
 
-    String UPDATE_USUARIO = "kjvargas.updateUsuario";
+    String HABILITAR_USUARIO = SCHEMA+"habilitar_usuario";
 
-    String FIND_ALL_ROLES = "kjvargas.find_all_rol";
+    String CREATE_USUARIO = SCHEMA+"createUsuario";
 
-    String FIND_ALL_ROLES_NO_ASIGNADO_USER = "kjvargas.roles_no_asignado_user";
+    String UPDATE_USUARIO = SCHEMA+"updateUsuario";
 
-    String FIND_ALL_ROLES_BY_ID = "kjvargas.find_all_rol_by_id";
+    String FIND_ALL_ROLES = SCHEMA+"find_all_rol";
 
-    String FIND_USERS_BY_ID = "kjvargas.find_users_by_id";
+    String FIND_ALL_ROLES_NO_ASIGNADO_USER = SCHEMA+"roles_no_asignado_user";
 
-    String UPDATE_ROL_USER = "kjvargas.update_rol_user";
+    String FIND_ALL_ROLES_BY_ID = SCHEMA+"find_all_rol_by_id";
 
-    String FIND_USER_BY_EMAIL = "kjvargas.find_user_by_email";
+    String FIND_USERS_BY_ID = SCHEMA+"find_users_by_id";
 
-    String FIND_USER_BY_EMAIL_LOAD = "kjvargas.find_user_by_email_load";
+    String UPDATE_ROL_USER = SCHEMA+"update_rol_user";
+
+    String FIND_USER_BY_EMAIL = SCHEMA+"find_user_by_email";
+
+    String FIND_USER_BY_EMAIL_LOAD = SCHEMA+"find_user_by_email_load";
 }
