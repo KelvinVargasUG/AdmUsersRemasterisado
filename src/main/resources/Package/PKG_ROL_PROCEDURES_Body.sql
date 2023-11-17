7e942e2b68388cf2c9f879cf359a420143ba5e24
VARGAS CHAFLE KELVIN JOSUE <kvargas@sasf.local>
2023-11-16 19:49:19 -0500

    Update

--- src/main/resources/Package/PKG_ROL_PROCEDURES_Body.sql
+++ src/main/resources/Package/PKG_ROL_PROCEDURES_Body.sql
@@ -5,7 +5,10 @@ CREATE OR REPLACE PACKAGE BODY pkg_rol_procedures AS
                rol.nombre,
                rol.descripcion
         FROM rol rol
-        WHERE rol.estado IS NOT NULL; END find_all_rol; PROCEDURE roles_no_asignado_user (p_id_user IN NUMBER, p_rol OUT SYS_REFCURSOR) IS BEGIN OPEN p_rol
+        WHERE rol.estado IS NOT NULL;
+        END find_all_rol;
+
+        PROCEDURE roles_no_asignado_user (p_id_user IN NUMBER, p_rol OUT SYS_REFCURSOR) IS BEGIN OPEN p_rol
         FOR
         SELECT r.id_rol,
                r.nombre
@@ -16,7 +19,10 @@ CREATE OR REPLACE PACKAGE BODY pkg_rol_procedures AS
              SELECT ur.id_rol
              FROM usuario_rol ur WHERE ur.estado IS NOT NULL
                                    AND ur.id_usuario = p_id_user ) tx
-                INNER JOIN rol r ON r.id_rol = tx.id_rol; END roles_no_asignado_user; PROCEDURE find_all_rol_by_id (p_id_user IN NUMBER, p_rol OUT SYS_REFCURSOR) IS BEGIN OPEN p_rol
+                INNER JOIN rol r ON r.id_rol = tx.id_rol;
+        END roles_no_asignado_user;
+
+        PROCEDURE find_all_rol_by_id (p_id_user IN NUMBER, p_rol OUT SYS_REFCURSOR) IS BEGIN OPEN p_rol
         FOR
         SELECT ur.id_rol,
                r.nombre,
